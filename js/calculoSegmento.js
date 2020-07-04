780c86811ace405d8791c9bd41756cf09107a186
Carlos Eduardo <carlos505monks@hotmail.com>
2020-07-04 00:36:36 -0300

    correcao de soma das desepesas

--- js/calculoSegmento.js
+++ js/calculoSegmento.js
@@ -1,31 +1,47 @@
 jQuery(document).ready(function(){
     jQuery('input').on('keyup',function(){
         if(jQuery(this).attr('name') === 'resultadoCasaApartamento'){
-        return false;
+            return false;
         }
-        
-        const aluguelFinanciamento = (jQuery('#aluguelFinanciamento').val() == '' ? 0 : jQuery('#aluguelFinanciamento').val().replace(',', '.'));
-        const condominio = (jQuery('#condominio').val() == '' ? 0 : jQuery('#condominio').val().replace(',', '.'));
-        const iptu = (jQuery('#iptu').val() == '' ? 0 : jQuery('#iptu').val().replace(',', '.'));
-        const agua = (jQuery('#agua').val() == '' ? 0 : jQuery('#agua').val().replace(',', '.'));
-        const luz = (jQuery('#luz').val() == '' ? 0 : jQuery('#luz').val().replace(',', '.'));
-        const gas = (jQuery('#gas').val() == '' ? 0 : jQuery('#gas').val().replace(',', '.'));
-        const internet = (jQuery('#internet').val() == '' ? 0 : jQuery('#internet').val().replace(',', '.'));
-        const telefone = (jQuery('#telefone').val() == '' ? 0 : jQuery('#telefone').val().replace(',', '.'));
-        const tvacabo = (jQuery('#tvacabo').val() == '' ? 0 : jQuery('#tvacabo').val().replace(',', '.'));
-        const outrosCasaApartamento = (jQuery('#outrosCasaApartamento').val() == '' ? 0 : jQuery('#outrosCasaApartamento').val().replace(',', '.'));
-        
-        var resultadoCasaApartamento = 
-        (
-            parseFloat(aluguelFinanciamento) + parseFloat(condominio) +
-            parseFloat(iptu) + parseFloat(agua) +
-            parseFloat(luz) + parseFloat(gas) +
-            parseFloat(internet) + parseFloat(telefone) + 
-            parseFloat(tvacabo) + parseFloat(outrosCasaApartamento)
-        );
-        debugger;
-        
-        $('#resultadoCasaApartamento').html(
-            resultadoCasaApartamento.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
+
+        var aluguelFinanciamento = $('#aluguelFinanciamento').maskMoney('unmasked')[0];
+        var condominio = $('#condominio').maskMoney('unmasked')[0];
+        var iptu = $('#iptu').maskMoney('unmasked')[0];
+        var agua = $('#agua').maskMoney('unmasked')[0];
+        var luz = $('#luz').maskMoney('unmasked')[0];
+        var gas = $('#gas').maskMoney('unmasked')[0];
+        var internet = $('#internet').maskMoney('unmasked')[0];
+        var telefone = $('#telefone').maskMoney('unmasked')[0];
+        var tvacabo = $('#tvacabo').maskMoney('unmasked')[0];
+        var outrosCasaApartamento = $('#outrosCasaApartamento').maskMoney('unmasked')[0];
+
+        var despesaSoma = 
+        currency(aluguelFinanciamento).
+        add(condominio).
+        add(iptu).
+        add(agua).
+        add(luz).
+        add(gas).
+        add(internet).
+        add(telefone).
+        add(tvacabo).
+        add(outrosCasaApartamento);
+
+        var optionBRL = { style: 'currency', currency: 'BRL' };
+        var formatBRL = new Intl.NumberFormat('pt-BR', optionBRL);
+        $('#resultadoCasaApartamento').html(formatBRL.format(despesaSoma));
     });
 });
+
+$(function() {
+    $('#aluguelFinanciamento').maskMoney();
+    $('#condominio').maskMoney();
+    $('#iptu').maskMoney();
+    $('#agua').maskMoney();
+    $('#luz').maskMoney();
+    $('#gas').maskMoney();
+    $('#internet').maskMoney();
+    $('#telefone').maskMoney();
+    $('#tvacabo').maskMoney();
+    $('#outrosCasaApartamento').maskMoney();
+})
